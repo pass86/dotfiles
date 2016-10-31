feedf532013d2e9db184f4bc69e35ec3c150b594
pass86 <pass86@gmail.com>
2016-11-01 01:28:05 +0800

    add update.sh for git submodule update

---
+++ bin/update.sh
@@ -0,0 +1,14 @@
+git submodule update --init vim/bundle/a.vim
+git submodule update --init vim/bundle/ack.vim
+git submodule update --init vim/bundle/auto-pairs
+git submodule update --init vim/bundle/ctrlp.vim
+git submodule update --init vim/bundle/gundo.vim
+git submodule update --init vim/bundle/mru
+git submodule update --init vim/bundle/nerdcommenter
+git submodule update --init vim/bundle/nerdtree
+git submodule update --init vim/bundle/supertab
+git submodule update --init vim/bundle/ultisnips
+git submodule update --init vim/bundle/vim-airline
+git submodule update --init vim/bundle/vim-bookmarks
+git submodule update --init vim/bundle/vim-reveal-in-finder
+git submodule update --init vim/bundle/vim-startify
