739022976ef9a604e9cccc941ca236ab03c86645
pass86 <pass86@gmail.com>
2016-11-01 10:15:01 +0800

    clean code

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -46,6 +46,3 @@ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #cd ~/.oh-my-zsh/custom/plugins
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-
-# finish
-cd ~
