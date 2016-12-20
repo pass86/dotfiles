f91cec6dca42c64d6978b1d5c1085282d5c3d5b1
pass86 <pass86@gmail.com>
2016-12-21 00:23:55 +0800

    just yum install

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,9 +1,5 @@
 # package
-sudo yum install -y epel-release git zsh tmux the_silver_searcher tree psmisc htop iotop iftop dstat ctags net-tools wget bzip2 unzip p7zip autoconf automake libtool gcc-c++ ncurses-devel python-devel zlib-devel bzip2-devel libquadmath-devel libevent-devel openssl-devel
-
-# dir
-mkdir -p ~/code
-mkdir -p ~/repo
+sudo yum install -y epel-release git zsh the_silver_searcher tree psmisc htop iotop iftop dstat ctags net-tools wget bzip2 unzip p7zip autoconf automake libtool gcc-c++ ncurses-devel python-devel zlib-devel bzip2-devel libquadmath-devel libevent-devel
 
 # git
 git config --global user.name pass86
@@ -19,15 +15,19 @@ git config --global alias.st status
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.unstage 'reset HEAD --'
 
+# dir
+#mkdir -p ~/code
+#mkdir -p ~/repo
+
 # dotfiles
-git clone https://github.com/pass86/dotfiles.git ~
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-rm ~/dotfiles/.bash_profile
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+#git clone https://github.com/pass86/dotfiles.git ~
+#ln -s ~/dotfiles/.vimrc ~/.vimrc
+#ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+#rm ~/dotfiles/.bash_profile
+#ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 
 # vimwiki
-git clone https://github.com/pass86/vimwiki.git ~
+#git clone https://github.com/pass86/vimwiki.git ~
 
 # vim
 #git clone https://github.com/vim/vim.git ~/code
@@ -45,8 +45,8 @@ git clone https://github.com/pass86/vimwiki.git ~
 #sudo make install
 
 # plugin
-cd ~/dotfiles
-./bin/update.sh
+#cd ~/dotfiles
+#./bin/update.sh
 #git submodule update --init --recursive
 
 # ycm
@@ -54,5 +54,5 @@ cd ~/dotfiles
 #./install.py --clang-completer
 
 # oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins
