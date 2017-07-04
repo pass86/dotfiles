c825c22c88e62b8cec75b155482a2e3418f20bae
pass86 <pass86@gmail.com>
2017-07-04 21:10:57 +0800

    add setln.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,6 @@
 # package
-sudo yum install -y epel-release git zsh the_silver_searcher tree psmisc htop iotop iftop dstat ctags net-tools wget bzip2 unzip p7zip autoconf automake libtool gcc-c++ ncurses-devel python-devel zlib-devel bzip2-devel libquadmath-devel libevent-devel tcl
+sudo yum install -y epel-release
+sudo yum install -y git zsh the_silver_searcher tree psmisc htop iotop iftop dstat ctags net-tools wget bzip2 unzip p7zip autoconf automake libtool gcc-c++ ncurses-devel python-devel zlib-devel bzip2-devel libquadmath-devel libevent-devel tcl
 
 # git
 git config --global user.name pass86
@@ -14,44 +15,3 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.unstage 'reset HEAD --'
-
-# dir
-#mkdir -p ~/code
-#mkdir -p ~/repo
-
-# dotfiles
-#git clone https://github.com/pass86/dotfiles.git ~
-#ln -s ~/dotfiles/.vimrc ~/.vimrc
-#ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-#rm ~/dotfiles/.bash_profile
-#ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-
-# vimwiki
-#git clone https://github.com/pass86/vimwiki.git ~
-
-# vim
-#git clone https://github.com/vim/vim.git ~/code
-#cd ~/code/vim
-#./configure --enable-pythoninterp=yes --without-python-config-dir
-#make
-#sudo make install
-
-# tmux
-#git clone https://github.com/tmux/tmux.git ~/code
-#cd ~/code/tmux
-#./autogen.sh
-#./configure
-#make
-#sudo make install
-
-# plugin
-#cd ~/dotfiles
-#./bin/update.sh
-#git submodule update --init --recursive
-
-# ycm
-#cd ~/dotfiles/vim/bundle/YouCompleteMe
-#./install.py --clang-completer
-
-# oh my zsh
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
---
+++ bin/setln.sh
@@ -0,0 +1,3 @@
+ ln -s ~/dotfiles/.vimrc ~/.vimrc
+ ln -s ~/dotfiles/.zshrc ~/.zshrc
+ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
