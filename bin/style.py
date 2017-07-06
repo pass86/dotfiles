b45c706d933c1a20344730b1666e6b1964d35423
pass86 <pass86@gmail.com>
2017-07-06 18:03:15 +0800

    remove empty trailing

--- bin/style.py
+++ bin/style.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os 
+import os
 import sys
 import codecs
 import shutil
 import subprocess
 
-def style(root): 
-    for files in os.listdir(root): 
-        path = os.path.join(root, files) 
+def style(root):
+    for files in os.listdir(root):
+        path = os.path.join(root, files)
         ext = os.path.splitext(path)[1]
         if ext == ".cs" or ext == ".c" or ext == ".cc" or ext == ".cpp" or ext == ".h" or ext == ".hpp" or ext == ".java":
             if path.endswith(".pb.h") or path.endswith(".pb.cc"):
@@ -22,13 +22,13 @@ def style(root):
             except UnicodeDecodeError:
                 ori_file.close()
                 utf8_path = path + ".utf8"
-                utf8_file = open(utf8_path, "w") 
+                utf8_file = open(utf8_path, "w")
                 subprocess.call(["iconv", "-f", "gbk", "-t", "utf-8", "-c", path], stdout=utf8_file)
                 utf8_file.close()
                 os.remove(path)
                 shutil.move(utf8_path, path)
             subprocess.call(["astyle", "--formatted", "--style=google", "--ascii", "--suffix=none", "--indent=spaces=4", "--lineend=linux", "--add-brackets", path])
-        if os.path.isdir(path): 
-            style(path) 
+        if os.path.isdir(path):
+            style(path)
 
 style(sys.argv[1])
--- bin/tags.py
+++ bin/tags.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os 
+import os
 import sys
 import codecs
 import shutil
 import subprocess
 
-def tags(root): 
+def tags(root):
     precwd = os.getcwd()
     os.chdir(root)
     #subprocess.call(["cscope", "-Rb"])
