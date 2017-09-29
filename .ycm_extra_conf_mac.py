b27fdfbfbac62c20bce42d35a6777b70557da9c6
pass86 <pass86@gmail.com>
2017-09-29 19:55:20 +0800

    update to xcode9

--- .ycm_extra_conf_mac.py
+++ .ycm_extra_conf_mac.py
@@ -44,7 +44,7 @@ flags = [
 '-I/usr/local/mysql/include',
 # echo | clang -stdlib=libc++ -v -E -x c++ -
 '-isystem/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1',
-'-isystem/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/8.1.0/include',
+'-isystem/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/9.0.0/include',
 '-isystem/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
 '-isystem/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include',
 '-isystem/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks',
