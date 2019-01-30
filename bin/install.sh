2f8bbbe410a57526638d43dee786445eed807ca2
pass86 <pass86@gmail.com>
2019-01-30 21:34:25 +0800

    Add install.sh

---
+++ bin/install.sh
@@ -0,0 +1,4 @@
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/zsh/themes/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
