e91f18a32ed3441c2e9672cb69084de97c1a6385
pass86 <pass86@gmail.com>
2019-02-06 14:29:09 +0800

    Add space for |

--- bin/svn_missing.sh
+++ bin/svn_missing.sh
@@ -1,1 +1,1 @@
-svn st | grep ! | cut -d! -f2| sed 's/^ *//' | sed 's/^/"/g' | sed 's/$/"/g' | xargs svn rm
+svn st | grep ! | cut -d! -f2 | sed 's/^ *//' | sed 's/^/"/g' | sed 's/$/"/g' | xargs svn rm
