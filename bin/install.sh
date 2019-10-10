5d02daef760756c72d2a1b551863dae493a7430f
Jimmy Yuan <pass86@gmail.com>
2019-10-11 01:26:55 +0800

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -3,5 +3,11 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/zsh/themes/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
-ln -sf ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+if [[ -d ~/.oh-my-zsh/themes ]]; then
+    ln -sf ~/dotfiles/zsh/themes/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+fi
+
+if [[ -d ~/.config/alacritty ]]; then
+    ln -sf ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+fi
