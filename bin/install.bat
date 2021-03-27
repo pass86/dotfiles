93629a46092ffa0582aad9d283a4570501e27902
Jimmy Yuan <pass86@gmail.com>
2021-03-27 14:32:27 +0800

    Update install.bat

--- bin/install.bat
+++ bin/install.bat
@@ -1,2 +1,4 @@
 mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\.vimrc
 mklink %USERPROFILE%\_ideavimrc %USERPROFILE%\dotfiles\.ideavimrc
+mklink %USERPROFILE%\AppData\Roaming\Code\User\settings.json %USERPROFILE%\dotfiles\vscode\settings.json
+mklink %USERPROFILE%\AppData\Roaming\Code\User\keybindings.json %USERPROFILE%\dotfiles\vscode\keybindings.json
