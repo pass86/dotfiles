ea9dd06bf22d1a9dfac2cf0f06fde1261a5c6eaf
Jimmy Yuan <pass86@gmail.com>
2021-06-12 23:20:48 +0800

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -2,15 +2,11 @@
 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 
 if [ -d ~/.oh-my-zsh/themes ]; then
     ln -sf ~/dotfiles/zsh/themes/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
-fi
-
-if [ -d ~/.config/alacritty ]; then
-    ln -sf ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 fi
 
 if [ -d ~/Library/Application\ Support/Code/User ]; then
