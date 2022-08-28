346a49299390e6fe5f1636da2cd5c122f66fc71a
Jimmy Yuan <pass86@gmail.com>
2022-08-28 18:27:24 +0800

    Update ycm_upgrade.sh

--- bin/ycm_upgrade.sh
+++ bin/ycm_upgrade.sh
@@ -2,3 +2,11 @@
 
 git submodule update --remote vim/plugins/YouCompleteMe
 git submodule update --remote vim/plugins/ultisnips
+
+cd vim/plugins/YouCompleteMe
+git submodule update --init --recursive
+cd - > /dev/null
+
+cd vim/plugins/YouCompleteMe/third_party/ycmd
+git submodule update --init --recursive
+cd - > /dev/null
