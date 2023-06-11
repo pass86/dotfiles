295cbe915a39cc01b1892204f143ede4cd79828d
Jimmy Yuan <pass86@gmail.com>
2023-06-11 15:54:25 +0800

    Add .i3status.conf

--- bin/install.sh
+++ bin/install.sh
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+ln -sf ~/dotfiles/.i3status.conf ~/.i3status.conf
 
 if [ -d ~/.oh-my-zsh/themes ]; then
     ln -sf ~/dotfiles/zsh/themes/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
