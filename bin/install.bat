a5c79d95758c6bf9d673549c10c00ab9d96d00c3
Jimmy Yuan <pass86@gmail.com>
2025-06-28 17:29:40 +0800

    Install Profile.ps1

--- bin/install.bat
+++ bin/install.bat
@@ -1,2 +1,5 @@
 mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\.vimrc
 mklink %USERPROFILE%\_ideavimrc %USERPROFILE%\dotfiles\.ideavimrc
+
+mkdir %USERPROFILE%\Documents\PowerShell
+mklink %USERPROFILE%\Documents\PowerShell\Profile.ps1 %USERPROFILE%\dotfiles\Profile.ps1
