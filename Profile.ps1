ebdd648b9ab0423be147c057cd44a0fb4d273445
Jimmy Yuan <pass86@gmail.com>
2025-07-02 22:10:13 +0800

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -1,1 +1,3 @@
-Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord Ctrl+f -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord Ctrl+p -Function PreviousHistory
+Set-PSReadLineKeyHandler -Chord Ctrl+n -Function NextHistory
