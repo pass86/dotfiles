f02209853a3908142e3908ae91c773805e125d57
Jimmy Yuan <pass86@gmail.com>
2026-08-22 19:19:49 +0800

    Add GlobalSettingsStorage.DotSettings

--- bin/install.bat
+++ bin/install.bat
@@ -3,3 +3,6 @@ mklink %USERPROFILE%\_ideavimrc %USERPROFILE%\dotfiles\.ideavimrc
 
 mkdir %USERPROFILE%\Documents\PowerShell
 mklink %USERPROFILE%\Documents\PowerShell\Profile.ps1 %USERPROFILE%\dotfiles\Profile.ps1
+
+mkdir %APPDATA%\JetBrains\Shared\vAny
+mklink %APPDATA%\JetBrains\Shared\vAny\GlobalSettingsStorage.DotSettings %USERPROFILE%\dotfiles\GlobalSettingsStorage.DotSettings
